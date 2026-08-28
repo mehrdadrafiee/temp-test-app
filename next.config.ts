3f8ed19aa2d53768ede84e35f55f9a144717ddff
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2026-08-28 16:19:39 -0700

    fix next config for base path

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-    images: {
-        unoptimized: true,
-    },
-    output: 'export',
+  output: "export",
+  basePath: process.env.CACHET_BASE_PATH || "",
+  trailingSlash: true,
+  images: { unoptimized: true },
 }
 
 export default nextConfig
