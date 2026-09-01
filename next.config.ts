ae3f20563abb75702f4011007481b0f970e911ff
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2026-08-31 23:36:37 -0700

    trigger engine by peyk base path

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.CACHET_BASE_PATH || "",
+  basePath: process.env.PEYK_BASE_PATH || "",
   trailingSlash: true,
   images: { unoptimized: true },
 }
